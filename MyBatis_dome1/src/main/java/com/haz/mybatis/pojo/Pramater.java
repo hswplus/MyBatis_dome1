1da75b2f74bbac452cf6c8ce743cd690a5cbfced
HAZ <wen1275@qq.com>
2022-03-26 17:28:15 +0800

    try again

--- MyBatis_dome1/src/main/java/com/haz/mybatis/pojo/Pramater.java
+++ MyBatis_dome1/src/main/java/com/haz/mybatis/pojo/Pramater.java
@@ -4,5 +4,7 @@ public class Pramater {
     public static void main(String[] args) {
         System.out.println("Hello git！");
         System.out.println("Hello git！");
+        System.out.println("Hello git！");
+
     }
 }
